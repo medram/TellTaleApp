96a775c05df2840367b491b0a723ee9103d40542
medram <mramouchy@gmail.com>
2021-07-20 17:07:44 +0100

    Update: Screens added and updated

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,6 +1,49 @@
 import * as React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, ScrollView, Image } from 'react-native'
+import { useTheme } from 'react-native-elements'
+import Input from '../components/Input'
+import Button from '../components/Button'
+import { AuthContext } from '../utils/contexts'
+
 
 export default function LoginScreen(props) {
-    return <Text>Login</Text>
+    const { signIn, isLoading } = React.useContext(AuthContext)
+
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+
+
+    return (
+        <ScrollView contentContainerStyle={{ flex: 1 }}>
+        <View style={styles.container}>
+            <Image source={require("../assets/images/logo.png")} style={styles.logo} resizeMode="contain" />
+            <View style={styles.loginBox}>
+                <Input placeholder='Email' autoCapitalize='none' keyboardType='email-address' autoCompleteType='email' onChangeText={setEmail} />
+
+                <Input placeholder='Password' secureTextEntry={true} autoCapitalize='none' onChangeText={setPassword} />
+
+                <Button title='Sign In' onPress={() => signIn(email, password)} loading={isLoading} />
+            </View>
+        </View>
+    </ScrollView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    scrollViewContainer: {
+        flex: 1,
+    },
+    logo: {
+        height: 40,
+        marginVertical: 15,
+    },
+    loginBox: {
+        marginVertical: 10,
+        width: 300,
+    },
+})
---
+++ src/screens/OnlinePage.js
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { WebView } from 'react-native-webview'
+import { getGamesDetailt } from '../services/common-service'
+
+export default function OnlinePage(props)
+{
+    const { route, navigation , ...rest} = props
+    const { url } = route.params
+    const [content, setContent] = React.useState("")
+
+    React.useEffect(() => {
+        (async () => {
+            setContent(await getGamesDetailt(url))
+        })()
+    }, [])
+
+
+    return <WebView originWhitelist={['*']} source={{ html: content }} />
+}
+
+const styles = StyleSheet.create({
+
+})
